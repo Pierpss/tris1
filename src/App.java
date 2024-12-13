43fef5aa0105c0aec44dbf9da6abc2eb20139b94
Pierpss <pierpaolovalentini24@gmail.com>
2024-12-13 12:07:43 +0100

    passo 2

--- src/App.java
+++ src/App.java
@@ -1,4 +1,7 @@
 //passo 1: gioca solo l'utente. Il programma chiede in che posizione (riga,colonna) inserire la X e viene mostrata la griglia con la X nel posto indicato. Le caselle vuote mostreranno invece il simbolo "-"
+/*passo 2:  ripetere l'inserimento fino a quando non si completano tutte le caselle.
+Verificare che l'utente non scelga una casella già occupata oppure non valida (es: riga 5, colonna 0).
+Dopo ogni iterazione ristampare la griglia*/
 
 import java.util.Scanner;
 
@@ -23,21 +26,51 @@ public class App
             System.out.println("");
         }
 
-        System.out.println("In che posizione vuoi inserire la x?");
-        System.out.println("Inserisci la riga");
-        int riga = input.nextInt()-1;
-        System.out.println("Inserisci la colonna");
-        int colonna = input.nextInt()-1;
+        int contatore=0;
+        boolean pieno=true;
+        boolean controllo=true;
+        int riga=0;
+        int colonna=0;
 
-        griglia[riga][colonna]="x";
-        
-        for(int i=0;i<griglia.length;i++)
+        while(pieno)
         {
-            for(int j=0;j<griglia[i].length;j++)
+            while (controllo) 
             {
-                System.out.print(griglia[i][j]);
+                System.out.println("In che posizione vuoi inserire la x?");
+                System.out.println("Inserisci la riga");
+                riga = input.nextInt()-1;
+                System.out.println("Inserisci la colonna");
+                colonna = input.nextInt()-1;
+                
+                if (riga<0 || riga>=griglia.length || colonna<0 || colonna>=griglia[0].length ) 
+                {
+                    System.out.println("Inserimenti errati");
+                    System.out.println("-------------------");
+                }
+                else if (griglia[riga][colonna].equals("x")) 
+                {
+                    System.out.println("Posizione occupata");
+                }
+                else controllo=false;
             }
-            System.out.println("");
+            controllo=true;
+
+            griglia[riga][colonna]="x";
+            contatore++;
+
+            for(int i=0;i<griglia.length;i++)
+            {
+                for(int j=0;j<griglia[i].length;j++)
+                {
+                    System.out.print(griglia[i][j]);
+                }
+                System.out.println("");
+            }
+            if (contatore==griglia.length*griglia[0].length) 
+            {
+                System.out.println("Griglia piena");
+                pieno=false;
+            }
         }
         input.close();
     }
